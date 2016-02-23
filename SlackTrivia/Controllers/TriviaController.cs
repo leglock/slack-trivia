2099a5e946abd9175431a6e4d1b147607b1c3e39
Patrick Hartman <patrickh@gmail.com>
2016-02-23 15:31:28 -0600

    Now hits question API and responsed to 'clue' and 'answer' keyword requests

--- SlackTrivia/Controllers/TriviaController.cs
+++ SlackTrivia/Controllers/TriviaController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Text.RegularExpressions;
 using System.Web.Http;
+using Newtonsoft.Json;
 using SlackTrivia.Models;
 
 namespace SlackTrivia.Controllers
@@ -12,13 +14,66 @@ namespace SlackTrivia.Controllers
     public class TriviaController : ApiController
     {
         static private string slack_token = "gzjGlKQMiZ7jDjeUUKhycJ5f";
+        static HttpClient client = new HttpClient();
+        private static Jeopardy.Clue game;
 
         public IHttpActionResult Beacon(SlackMessage message)
         {
             if (message.token != slack_token)
                 return Unauthorized();
 
+            string keyword = ParseKeyword(message.text);
+
+            if (keyword == "clue")
+            {
+                game = GetClue();
+                var text = new 
+                {
+                    title = game.category.title,
+                    text = game.question
+                };
+
+                return Content(HttpStatusCode.OK, text);
+            }
+
+            if (keyword == "answer")
+            {
+                var text = new 
+                {
+                    text = String.Format("The answer I was looking for was \"{0}\".", game.answer)
+                };
+
+                game = null;
+
+                return Content(HttpStatusCode.OK, text);
+            }
+
+
             return Content(HttpStatusCode.OK, new {text = message.user_name + "'s request processed at " + DateTime.Now});
         }
+
+        public Jeopardy.Clue GetClue()
+        {
+            List<Jeopardy.Clue> model;
+
+            var task = client.GetAsync("http://jservice.io/api/random/").Result;
+            var response = task.Content.ReadAsStringAsync();
+            model = JsonConvert.DeserializeObject<List<Jeopardy.Clue>>(response.Result);
+
+            return model.First();
+        }
+
+        public string ParseKeyword(string message)
+        {
+            string keyword = null;
+
+            try {
+	            keyword = Regex.Match(message, "(?>!trivia )([a-z0-9]+)", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Multiline).Groups[1].Value;
+            } catch (ArgumentException ex) {
+	            // Syntax error in the regular expression
+            }
+
+            return keyword;
+        } 
     }
 }
---
+++ SlackTrivia/Models/Jeopardy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlackTrivia.Models
+{
+    public class Jeopardy
+    {
+        public class Clue
+        {
+            public DateTime airdate { get; set; }
+            public string answer { get; set; }
+            public Category category { get; set; }
+            public int? category_id { get; set; }
+            public DateTime created_at { get; set; }
+            public int? game_id { get; set; }
+            public int? id { get; set; }
+            public int? invalid_count { get; set; }
+            public string question { get; set; }
+            public DateTime updated_at { get; set; }
+            public int? value { get; set; }
+        }
+
+        public class Category
+        {
+            public int? clues_count { get; set; }
+            public DateTime created_at { get; set; }
+            public int? id { get; set; }
+            public string title { get; set; }
+            public DateTime updated_at { get; set; }
+        }
+    }
+}
