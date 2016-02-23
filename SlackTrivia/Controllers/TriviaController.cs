1471d36d9805069efa2ae431f6f0239add52902c
Patrick Hartman <patrickh@gmail.com>
2016-02-22 20:17:44 -0600

    Code for handling incoming Slack message and returning placeholder response

---
+++ SlackTrivia/Controllers/TriviaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using SlackTrivia.Models;
+
+namespace SlackTrivia.Controllers
+{
+    public class TriviaController : ApiController
+    {
+        static private string slack_token = "gzjGlKQMiZ7jDjeUUKhycJ5f";
+
+        public IHttpActionResult Beacon(SlackMessage message)
+        {
+            if (message.token != slack_token)
+                return Unauthorized();
+
+            return Content(HttpStatusCode.OK, new {text = message.user_name + "'s request processed at " + DateTime.Now});
+        }
+    }
+}
